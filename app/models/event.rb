46dbb0083f4ec7ad2765e6be10505d46558b30b5
iamm28 <alisonmackay246@gmail.com>
2018-02-12 16:08:54 -0500

    started event crud

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :location
-  has_many :invites
-  has_many :users, through: :invites 
+  has_many :invites, dependent: :destroy
+  has_many :users, through: :invites
 end
